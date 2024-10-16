41420a51206856e2bd387a19ea4d794613efb8cf
Zyrakia <ole.lammers@pm.me>
2024-10-16 10:41:38 -0700

    added back removed title statistics methods

--- src/main/ca/bcit/comp2522/lab06/BookStore.java
+++ src/main/ca/bcit/comp2522/lab06/BookStore.java
@@ -1,6 +1,7 @@
 package ca.bcit.comp2522.lab06;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -46,6 +47,121 @@ public class BookStore<T extends Literature> {
         for (final T item : this.items) {
             System.out.println(item.getTitle());
         }
+    }
+
+    /**
+     * Prints out the titles of all literature pieces in this book store in
+     * upper case format.
+     */
+    public final void printAllTitles() {
+        for (final T lit : this.items) {
+            System.out.println(lit.getTitle().toUpperCase());
+        }
+    }
+
+    /**
+     * Prints all the titles that contain the specified title part.
+     *
+     * @param filterTitle the title to filter by
+     */
+    public final void printBookTitle(final String filterTitle) {
+        for (final T lit : this.items) {
+            final String title;
+            title = lit.getTitle();
+
+            if (!title.contains(filterTitle)) {
+                continue;
+            }
+
+            System.out.println(title);
+        }
+    }
+
+    /**
+     * Prints the titles of all books in this book store in alphabetical order.
+     */
+    public final void printTitlesInAlphaOrder() {
+        Collections.sort(this.items);
+
+        for (final T lit : this.items) {
+            System.out.println(lit.getTitle());
+        }
+    }
+
+    /**
+     * Returns the longest title in of all the literature piees in this book
+     * store.
+     *
+     * @return the longest title, or null if there are no books
+     */
+    public final String getLongest() {
+        T longestLit = null;
+        int longestLength = Integer.MIN_VALUE;
+
+        for (final T lit : this.items) {
+            final int len = lit.getTitle().length();
+
+            if (longestLit == null) {
+                longestLit = lit;
+                longestLength = len;
+                continue;
+            }
+
+            if (len > longestLength) {
+                longestLit = lit;
+                longestLength = len;
+            }
+        }
+
+        if (longestLit == null) {
+            return null;
+        }
+
+        return longestLit.getTitle();
+    }
+
+    /**
+     * Returns the amount of literature pieces that have the specified word in
+     * the title.
+     *
+     * @param word the word to check for
+     * @return the amount of books containing that word (case-sensitive)
+     */
+    public final int howManyBooksContain(final String word) {
+        final String lowerWord = word.toLowerCase();
+
+        int count = 0;
+        for (final T lit : this.items) {
+            final String lowerTitle;
+
+            lowerTitle = lit.getTitle().toLowerCase();
+
+            if (lowerTitle.contains(lowerWord)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    /**
+     * Returns a list of all the literature pieces that are of the specified
+     * title length.
+     *
+     * @param titleLength the title length to filter by
+     * @return the literature pieces matching that title length
+     */
+    public final List<T> getBooksThisLength(final int titleLength) {
+        final List<T> matchingLengthNovels;
+
+        matchingLengthNovels = new ArrayList<>();
+
+        for (final T lit : this.items) {
+            final int len = lit.getTitle().length();
+            if (len == titleLength) matchingLengthNovels.add(lit);
+        }
+
+        return matchingLengthNovels;
     }
 
     /**
